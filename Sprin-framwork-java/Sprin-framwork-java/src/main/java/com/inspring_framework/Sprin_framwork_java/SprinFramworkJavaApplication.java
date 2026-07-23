ab72c8a3fe89a3eddbc364a3b2549256986af4d0
Neha Thakare <neha.thakare1104@gmail.com>
2026-07-23 22:45:37 +0530

    Committing java files:w

---
+++ Sprin-framwork-java/Sprin-framwork-java/src/main/java/com/inspring_framework/Sprin_framwork_java/SprinFramworkJavaApplication.java
@@ -0,0 +1,13 @@
+package com.inspring_framework.Sprin_framwork_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinFramworkJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinFramworkJavaApplication.class, args);
+	}
+
+}
---
+++ Sprin-framwork-java/Sprin-framwork-java/src/test/java/com/inspring_framework/Sprin_framwork_java/SprinFramworkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inspring_framework.Sprin_framwork_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinFramworkJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
